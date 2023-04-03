c487265d425bc20cd6a3db7338a612ef23a8bb9c
Enes <enes431811@gmail.com>
2023-04-03 17:23:11 +0300

    Project has been completed.

--- app.js
+++ app.js
@@ -4,33 +4,92 @@
 //--------------------------------------------------------
 //----------------------------------------------------
 
+let isError=false;
+
+const getCountries = async function (searchCountry) {
+
+    //const selectedCountry=document.querySelector();
+
+    //const url=`https://restcountries.com/v3.1/name/${selectedCountry}`
+
+    const url = `https://restcountries.com/v3.1/name/${searchCountry}`
 
 
-const getCountries=async function(searchCountry){
+    try {
+        const res = await fetch(url);
 
- //const selectedCountry=document.querySelector();
+        if (!res.ok) {
+            isError=true;
+        }
 
- //const url=`https://restcountries.com/v3.1/name/${selectedCountry}`
+        const data = await res.json();
+        renderCountries(data);
 
- const url=`https://restcountries.com/v3.1/name/${searchCountry}`
- 
- const res=await fetch(url);
 
- const data=await res.json();
- 
- try {
-    
- } catch (error) {
-    
- }
+
+    } catch (error) {
+        isError=true;
+        console.log(error);
+    }
 
 
 
 };
 
-const searchBtn=document.querySelector(".btn-country");
+const searchBtn = document.querySelector(".btn-country");
 
-searchBtn.addEventListener("click",()=>{
-    const searchCountry=document.getElementById("search-country").value.trim();
-    getCountries(searchCountry)
+searchBtn.addEventListener("click", () => {
+    const searchCountry = document.getElementById("search-country").value.trim();
+    getCountries(searchCountry.toLowerCase());
 })
+
+const renderCountries = (data) => {
+    const {
+        capital,
+        currencies,
+        flags:{svg},
+        languages,
+        name:{common},
+        region
+
+    } = data[0];
+
+    const cardDiv=document.querySelector(".card")
+
+    
+    if (!isError) {
+    cardDiv.innerHTML =`
+    <img src="${svg}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${common}</h5>
+        <p class="card-text"><i class="fa-sharp fa-solid fa-map"></i> ${region}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><i class="fa-solid fa-house-flag"></i> ${capital}</li>
+        <li class="list-group-item"><i class="fa-solid fa-money-bill-wave"></i> ${Object.values(currencies)[0].name} ${Object.values(currencies)[0].symbol}</li>
+        <li class="list-group-item"><i class="fa-solid fa-language"></i> ${Object.values(languages)}</li>
+    </ul>
+    <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+    </div>     
+    
+    `
+    }else{
+        cardDiv.innerHTML =`
+        
+        <h2> Country can not be found</h2>
+        <img src="./404.jpg" alt="">
+        
+        `
+    }
+    
+    
+}
+
+
+
+
+
+
+
